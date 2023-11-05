4c9c69cb58d567b673122764d0e4bce445812c27
benlaptop25 <ben.taylor2573@gmail.com>
2023-11-05 01:26:53 +0000

    added labels

--- f1_points_over_time.py
+++ f1_points_over_time.py
@@ -1,5 +1,4 @@
-import datetime
-import random
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 from data import dates
@@ -7,12 +6,15 @@ from data import alphatauri_points, williams_points, alfa_romeo_points, haas_poi
 
 
 # plot
-plt.plot(dates, alphatauri_points)
-plt.plot(dates, williams_points)
-plt.plot(dates, alfa_romeo_points)
-plt.plot(dates, haas_points)
+plt.plot(dates, alphatauri_points, '#010f1a', label="Scuderia AlphaTauri", linewidth=3)
+plt.plot(dates, williams_points, '#00a1dd', label="Williams", linewidth=3)
+plt.plot(dates, alfa_romeo_points, '#720a1e', label="Alfa Romeo", linewidth=3)
+plt.plot(dates, haas_points, '#db2416', label="Haas", linewidth=3)
+
+plt.axvline(x=datetime(2023, 7, 3), color='#000000', linestyle='--')
 
 # beautify the x-labels
 plt.gcf().autofmt_xdate()
 
+plt.legend()
 plt.show()
