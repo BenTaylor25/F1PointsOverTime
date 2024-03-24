9995cf547a502ac9d714185a381d5480b78b5127
benlaptop25 <ben.taylor2573@gmail.com>
2024-03-24 08:38:43 +0000

    refactored data source to use json file

--- data.py
+++ data.py
@@ -144,3 +144,82 @@ points_to_cumulative(alfa_romeo_points)
 
 haas_points = list(_haas_weekends.values())
 points_to_cumulative(haas_points)
+
+
+# old ^
+
+# new v
+import os, json
+
+DATA_PATH = "./points_data"
+
+def get_season_data(year):
+    filepath = f"{DATA_PATH}/{year}.json"
+
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            deserialised_json = json.load(f)
+
+        season = {
+            "year": deserialised_json["year"],
+            "dates": get_event_dates(deserialised_json),
+            "team_names": get_team_names(deserialised_json),
+            "team_colours": get_team_colours(deserialised_json),
+            "team_points": get_team_points(deserialised_json)
+        }
+
+    return season
+
+def get_event_dates(deserialised_json) -> list[datetime]:
+    year = deserialised_json["year"]
+
+    dates = []
+    for event in deserialised_json["weekends"]:
+        month = event["date"]["month"]
+        day = event["date"]["day"]
+
+        dates.append(datetime(year, month, day))
+
+    return dates
+
+def get_team_colours(deserialised_json) -> dict[str, str]:
+    teams = get_team_names(deserialised_json)
+
+    team_colours = dict()
+    for team in deserialised_json["teams"]:
+        team_colours[team["name"]] = team["hex_colour"]
+
+    return team_colours
+
+def get_team_points(deserialised_json) -> dict[str, list[int]]:
+    teams = get_team_names(deserialised_json)
+    weekends = deserialised_json["weekends"]
+
+    team_points = dict()
+
+    for team in teams:
+        team_points[team] = []
+
+    for weekend in weekends:
+        for team in teams:
+            team_points[team].append(weekend["points"][team])
+
+    for team in team_points:
+        cumulative(team_points[team])
+
+    return team_points
+
+def get_team_names(deserialised_json) -> list[str]:
+    teams = []
+
+    for team in deserialised_json["teams"]:
+        teams.append(team["name"])
+
+    return teams
+
+def cumulative(numbers: list):
+    for i in range (1, len(numbers)):
+        numbers[i] += numbers[i - 1]
+
+if __name__ == "__main__":
+    get_season_data(2023)
--- f1_points_over_time.py
+++ f1_points_over_time.py
@@ -4,12 +4,24 @@ import matplotlib.pyplot as plt
 from data import dates
 from data import alphatauri_points, williams_points, alfa_romeo_points, haas_points
 
-
 # plot
-plt.plot(dates, alphatauri_points, '#010f1a', label="Scuderia AlphaTauri", linewidth=3, marker='o')
-plt.plot(dates, williams_points, '#00a1dd', label="Williams", linewidth=3, marker='o')
-plt.plot(dates, alfa_romeo_points, '#720a1e', label="Alfa Romeo", linewidth=3, marker='o')
-plt.plot(dates, haas_points, '#db2416', label="Haas", linewidth=3, marker='o')
+# plt.plot(dates, alphatauri_points, '#010f1a', label="Scuderia AlphaTauri", linewidth=3, marker='o')
+# plt.plot(dates, williams_points, '#00a1dd', label="Williams", linewidth=3, marker='o')
+# plt.plot(dates, alfa_romeo_points, '#720a1e', label="Alfa Romeo", linewidth=3, marker='o')
+# plt.plot(dates, haas_points, '#db2416', label="Haas", linewidth=3, marker='o')
+
+from data import get_season_data
+
+season = get_season_data(2023)
+dates = season["dates"]
+
+for team_name in season["team_names"]:
+    team_colour = season["team_colours"][team_name]
+    team_points = season["team_points"][team_name]
+
+    print(team_colour)
+
+    plt.plot(dates, team_points, team_colour, label=team_name, linewidth=3, marker='o')
 
 plt.axvline(x=datetime(2023, 7, 3), color='#000000', linestyle='--')
 
