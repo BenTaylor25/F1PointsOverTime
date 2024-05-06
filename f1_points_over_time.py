18fa43553aa4557201d2e103caaa32bdbf9af195
benlaptop25 <ben.taylor2573@gmail.com>
2024-05-06 10:56:14 +0100

    rescaled y axis

--- f1_points_over_time.py
+++ f1_points_over_time.py
@@ -1,10 +1,23 @@
 from datetime import datetime
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 from data import get_season_data
 
 PLACEMENT_START = datetime(2023, 7, 3)
 PLACEMENT_END = datetime(2024, 8, 2)
+
+def highest_team_points(season) -> int:
+    highest = 0
+
+    for team_name in season["team_names"]:
+        team_points = season["team_points"][team_name]
+        team_highest = max(team_points)
+
+        highest = max(highest, team_highest)
+
+    return highest
 
 if __name__ == "__main__":
     season = get_season_data()
@@ -27,8 +40,11 @@ if __name__ == "__main__":
         plt.axvline(x=today, color='#000000', linestyle='--')
 
 
-    # beautify the x-labels
+    # Format the x-labels.
     plt.gcf().autofmt_xdate()
+
+    # Use whole numbers for y-axis.
+    plt.yticks(np.arange(0, highest_team_points(season) + 1, 1.0))
 
     plt.legend()
     plt.show()
